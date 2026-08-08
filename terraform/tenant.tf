664fd4941291742b5d174e2c46c65f3f648bc5a0
John Nolan <me@johnnolan.dev>
2026-08-08 19:13:42 +0100

    Format code for consistency in tenant.tf

--- terraform/tenant.tf
+++ terraform/tenant.tf
@@ -1,14 +1,14 @@
 data "azuread_client_config" "current" {}
 
 resource "azuread_authentication_strength_policy" "default_mfa" {
-	display_name = "Default MFA"
-	description  = "Baseline authentication strength policy for tenant-wide conditional access."
+  display_name = "Default MFA"
+  description  = "Baseline authentication strength policy for tenant-wide conditional access."
 
-	allowed_combinations = [
-		"fido2",
-		"microsoftAuthenticatorPush",
-		"sms",
-		"softwareOath",
-		"voice",
-	]
+  allowed_combinations = [
+    "fido2",
+    "microsoftAuthenticatorPush",
+    "sms",
+    "softwareOath",
+    "voice",
+  ]
 }
